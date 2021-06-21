24a34cd79b2f9e112f9b82975c930c24a61fe944
phandb72 <phandb72@gmail.com>
2021-06-21 18:31:25 -0500

    Initial Commit

---
+++ bugtracker/src/main/java/com/javaprojects/bugtracker/BugtrackerApplication.java
@@ -0,0 +1,13 @@
+package com.javaprojects.bugtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugtrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugtrackerApplication.class, args);
+	}
+
+}
---
+++ bugtracker/src/test/java/com/javaprojects/bugtracker/BugtrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaprojects.bugtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugtrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
