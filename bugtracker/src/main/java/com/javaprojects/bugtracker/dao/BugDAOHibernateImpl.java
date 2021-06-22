b442cf600dcd8adb852b1dd21b37001f0087fcc1
phandb72 <phandb72@gmail.com>
2021-06-21 22:28:54 -0500

    Implement the Bug DAO interface

---
+++ bugtracker/src/main/java/com/javaprojects/bugtracker/dao/BugDAOHibernateImpl.java
@@ -0,0 +1,43 @@
+package com.javaprojects.bugtracker.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.javaprojects.bugtracker.entity.Bug;
+
+@Repository
+public class BugDAOHibernateImpl implements BugDAO {
+	
+	// define field for entity manager
+	private EntityManager entityManager;
+	
+	// set up constructor injection
+	@Autowired
+	public BugDAOHibernateImpl(EntityManager entityManager) {
+		this.entityManager = entityManager;
+	}
+
+	@Override
+	@Transactional
+	public List<Bug> findAll() {
+		// get the current hibernate session
+		Session currentSession = entityManager.unwrap(Session.class);
+		
+		// create a query using native Hibernate
+		Query<Bug> query = currentSession.createQuery("from Bug", Bug.class);
+		
+		// execute the query and get result list
+		List<Bug> bugs = query.getResultList();
+		
+		// return the results
+		return bugs;
+	}
+
+}
