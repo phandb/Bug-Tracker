8012284fe76bc6c036e7c1260b978d2198ee7517
phandb72 <phandb72@gmail.com>
2021-06-21 21:41:00 -0500

    Specify JDBC Properties

---
+++ bugtracker/src/main/java/com/javaprojects/bugtracker/entity/Bug.java
@@ -0,0 +1,17 @@
+package com.javaprojects.bugtracker.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="bug")
+public class Bug {
+	// define fields
+	
+	// define constructors
+	
+	// define getter/setter
+	
+	// define to string
+	
+}
