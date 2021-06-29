81e0d5fb993dbe60995ed763429b7e0e1eeebaab
phandb72 <phandb72@gmail.com>
2021-06-28 23:51:01 -0500

    Get List of Bugs for View/bugs/html

--- bugtracker/src/main/java/com/javaprojects/bugtracker/controller/AppController.java
+++ bugtracker/src/main/java/com/javaprojects/bugtracker/controller/AppController.java
@@ -1,5 +1,40 @@
 package com.javaprojects.bugtracker.controller;
 
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.javaprojects.bugtracker.entity.Bug;
+import com.javaprojects.bugtracker.service.BugService;
+
+
+@Controller
+@RequestMapping("/bug-tracker")
 public class AppController {
+	
+	private BugService bugService;
+	
+	public AppController(BugService bugService) {
+		
+		this.bugService = bugService;
+	}
+	
+	/****************************************/
+	// Add mapping for get list of bugs
+	@GetMapping("/bugs")
+	public String getListBug(Model theModel) {
+		
+		// Get list of bugs from db
+		List<Bug> bugs = bugService.findAll();
+		
+		// Add the list to model
+		theModel.addAttribute("bugs", bugs);
+		
+		return "view/bugs.html";
+	}
+	
 
 }
