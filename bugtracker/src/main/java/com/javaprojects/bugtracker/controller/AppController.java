0f0f6c6ff812c8f1639448d9246dec0111ee6810
phandb72 <phandb72@gmail.com>
2021-06-30 22:16:39 -0500

    Create an Add New Bug Functionality

--- bugtracker/src/main/java/com/javaprojects/bugtracker/controller/AppController.java
+++ bugtracker/src/main/java/com/javaprojects/bugtracker/controller/AppController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.javaprojects.bugtracker.entity.Bug;
@@ -34,6 +36,34 @@ public class AppController {
 		theModel.addAttribute("bugs", bugs);
 		
 		return "view/bugs.html";
+	}
+	
+	// Add mapping for adding bug
+	@GetMapping("/showFormForAdd")  //  This appears in URL
+	public String showFormForAdd(Model theModel) {
+		
+		// create model attribute to bind form data
+		Bug theBug = new Bug();
+		
+		// Thymeleaf template will access "new-bug" data for binding form data
+		theModel.addAttribute("bug", theBug);
+		
+		// src/main/resources/templates/bug-tracker/bug-form.html
+		return "view/bug-form";  // this is location of html file
+	}
+	
+	// Processing to save bug
+	@PostMapping("/save")
+	public String saveBug(@ModelAttribute("bug") Bug theBug) {
+		// data binding "bug" passed from bug-form
+		
+		// save the bug
+		bugService.save(theBug);
+		
+		
+		// use a redirect to prevent duplicate submission
+		return "redirect:/bug-tracker/bugs";
+		
 	}
 	
 
