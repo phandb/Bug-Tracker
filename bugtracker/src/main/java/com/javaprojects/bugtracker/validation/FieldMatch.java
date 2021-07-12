0f2d1e07f6389152aa9e62837c2957b219b14ff9
phandb72 <phandb72@gmail.com>
2021-07-12 10:20:00 -0500

    Add Custom Validation for Password

---
+++ bugtracker/src/main/java/com/javaprojects/bugtracker/validation/FieldMatch.java
@@ -0,0 +1,33 @@
+package com.javaprojects.bugtracker.validation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+@Documented
+@Constraint(validatedBy = FieldMatchValidator.class)
+@Target({ElementType.TYPE,
+		ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface FieldMatch {
+
+	String message() default "";
+	Class<?>[] groups() default {};
+	Class<? extends Payload>[] payload() default {};
+	
+	String first();
+	String second();
+	
+	@Target({ ElementType.TYPE, ElementType.ANNOTATION_TYPE})
+	@Retention(RetentionPolicy.RUNTIME)
+	@Documented
+	@interface List
+	{
+		FieldMatch[] value();
+	}
+}
